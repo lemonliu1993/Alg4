06beabd9fbe9d1a660cf41acfa46babf57ad5c68
lemonliu1993 <2305247091@qq.com>
2018-04-25 23:30:01 +0800

    add q&a

--- Alg4/src/C1/BinarySearch.java
+++ Alg4/src/C1/BinarySearch.java
@@ -1,16 +1,34 @@
 package C1;
 
 public class BinarySearch {
-	public static int rank(int key,int[] a){
-		int lo=0;
-		int hi=a.length-1;
-		while(lo<=hi){
-			int mid=lo+(hi-lo)/2;
-			if(key<a[mid]) hi=mid-1;
-			else if(key>a[mid]) lo=mid+1;
-			else return mid;
+	public static int rank(int key, int[] a) {
+		int lo = 0;
+		int hi = a.length - 1;
+		while (lo <= hi) {
+			int mid = lo + (hi - lo) / 2;
+			if (key < a[mid])
+				hi = mid - 1;
+			else if (key > a[mid])
+				lo = mid + 1;
+			else
+				return mid;
 		}
 		return -1;
 	}
-	
+
+	public static int rank1(int key, int[] a) {
+		int lo = 0;
+		int hi = a.length - 1;
+		while (lo <= hi) {
+			// 被查找的键要么不存在，要么必然存在于a[lo..hi]之中。
+			int mid = lo + (hi - lo) / 2;
+			if (key < a[mid])
+				hi = mid - 1;
+			else if (key > a[mid])
+				lo = mid + 1;
+			else
+				return mid;
+		}
+		return -1;
+	}
 }
